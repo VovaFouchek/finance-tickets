77b9bac7c605fa323c8febc7ec2a982126d5aba5
Volodymyr Fouchek <vinvova555333@gmail.com>
2023-01-18 13:59:13 +0200

    Initialize project & add first setup

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => (
+	<div className="App">
+		<h1>Test2</h1>
+	</div>
+);
+
+export default App;
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,3 @@
+const Home = () => <div className="inner" />;
+
+export default Home;
